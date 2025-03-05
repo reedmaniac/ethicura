6ee77317000c98e78157f3f8ce53c7cdc925e167
Paul Burdick <paul@reedmaniac.com>
2025-03-04 20:15:55 -0700

    Sidebar menu items and dropdown options

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,7 +5,7 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { BookOpen, Folder, LayoutGrid, Plus } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
@@ -13,6 +13,36 @@ const mainNavItems: NavItem[] = [
         title: 'Dashboard',
         href: '/dashboard',
         icon: LayoutGrid,
+    },
+    {
+        title: 'Recalls',
+        href: '/dashboard/recalls',
+        icon: LayoutGrid,
+        actionMenu: {
+            icon: Plus,
+            title: 'Create Recall',
+            href: '/dashboard/recalls/create',
+        }
+    },
+    {
+        title: 'Corporations',
+        href: '/dashboard/corporations',
+        icon: LayoutGrid,
+        actionMenu: {
+            icon: Plus,
+            title: 'Create Corporation',
+            href: '/dashboard/corporations/create',
+        }
+    },
+    {
+        title: 'Users',
+        href: '/dashboard/users',
+        icon: LayoutGrid,
+        actionMenu: {
+            icon: Plus,
+            title: 'Create User',
+            href: '/dashboard/users/create',
+        }
     },
 ];
 
@@ -31,7 +61,7 @@ const footerNavItems: NavItem[] = [
 </script>
 
 <template>
-    <Sidebar collapsible="icon" variant="inset">
+    <Sidebar collapsible="icon" variant="inset" side="left">
         <SidebarHeader>
             <SidebarMenu>
                 <SidebarMenuItem>
--- resources/js/components/NavMain.vue
+++ resources/js/components/NavMain.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
-import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarMenuAction } from '@/components/ui/sidebar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import { type NavItem, type SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/vue3';
+import { MoreHorizontal } from 'lucide-vue-next';
 
 defineProps<{
     items: NavItem[];
@@ -21,6 +36,21 @@ const page = usePage<SharedData>();
                         <span>{{ item.title }}</span>
                     </Link>
                 </SidebarMenuButton>
+                <SidebarMenuAction as="a" v-if="item.actionMenu" :href="item.actionMenu.href" :title="item.actionMenu.title">
+                  <component :is="item.actionMenu.icon" /><span class="sr-only">{{ item.actionMenu.title }}</span>
+                </SidebarMenuAction>
+                <DropdownMenu v-else-if="item.dropdownMenus && item.dropdownMenus.length > 0">
+                    <DropdownMenuTrigger asChild>
+                        <SidebarMenuAction>
+                            <MoreHorizontal />
+                        </SidebarMenuAction>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent side="right" align="start">
+                        <DropdownMenuItem v-for="dropdownMenuItem in item.dropdownMenus" :key="dropdownMenuItem.title">
+                            <span>{{ dropdownMenuItem.title }}</span>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </SidebarMenuItem>
         </SidebarMenu>
     </SidebarGroup>
