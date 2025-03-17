1bec7105b84a899d94ca53d7c0041750517323b8
Paul Burdick <paul@reedmaniac.com>
2025-03-16 21:04:43 -0600

    Moving Dashboard code into controller

---
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use Inertia\Inertia;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    /**
+     * Show Main Dashboard
+     *
+     * @return \Inertia\Response
+     */
+    public function index(Request $request)
+    {
+        return Inertia::render('Dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,22 @@
 <?php
 
+use App\Http\Controllers\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::group(['as' => 'dashboard.', 'prefix' => 'dashboard'], function() {
+
+
+})->middleware(['auth', 'verified']);
+
+
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
