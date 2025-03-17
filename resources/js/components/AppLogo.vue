e8d88d938dd0bf9627c798b63d591f5a6c665ef1
Paul Burdick <paul@reedmaniac.com>
2025-03-17 10:35:53 -0600

    Improving the look of the Welcome splash page
    Eventually this will be a bit more advanced, but we gotta start somewhere

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -7,7 +7,7 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 <template>
     <div class="flex aspect-square size-8 items-center justify-center rounded-md">
-        <AppLogoIcon class="size-8 fill-current text-white dark:text-black" />
+        <AppLogoIcon class="size-8" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="mb-0.5 truncate font-semibold leading-none">{{ appName }}</span>
