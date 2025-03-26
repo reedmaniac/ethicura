a2ada73864c772bb0c71ff46c4ba9817ca76211f
Paul Burdick <paul@reedmaniac.com>
2025-03-26 10:57:20 -0700

    Tests kept on failing locally because of caching, so let’s just axe them all

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,4 +6,28 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = parent::createApplication();
+
+        $this->clearCache(); // NEW LINE -- Testing doesn't work properly with cached stuff.
+
+        return $app;
+    }
+
+    /**
+     * Clears Laravel Cache.
+     */
+    protected function clearCache()
+    {
+        $commands = ['clear-compiled', 'cache:clear', 'view:clear', 'config:clear', 'route:clear'];
+        foreach ($commands as $command) {
+            \Illuminate\Support\Facades\Artisan::call($command);
+        }
+    }
 }
